b66e046368a82aade2e2d8a4457dd8b0473a29dc
Nolence <rex.magana@gmail.com>
2021-07-12 15:00:41 -0600

    export extensions

--- packages/fireproof_riverpod/lib/fireproof_riverpod.dart
+++ packages/fireproof_riverpod/lib/fireproof_riverpod.dart
@@ -4,3 +4,4 @@ export 'src/models/collection_handler.dart';
 export 'src/models/paginated_collection_handler.dart';
 export 'src/models/paginated_query_handler.dart';
 export 'src/models/query_handler.dart';
+export 'src/utils/extensions.dart';
--- packages/fireproof_riverpod/lib/src/utils/extensions.dart
+++ packages/fireproof_riverpod/lib/src/utils/extensions.dart
@@ -1,19 +1,22 @@
-// extension AsyncConverter<T> on AsyncSnapshot<T> {
-//   AsyncValue<T> get toAsyncValue {
-//     if (hasError) {
-//       return AsyncError<T>(error!, stackTrace);
-//     }
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-//     // TODO: This isn't tested and possibly has subtle bugs
-//     if (connectionState == ConnectionState.waiting ||
-//         (connectionState == ConnectionState.none && !hasData)) {
-//       return const AsyncLoading();
-//     }
+extension AsyncConverter<T> on AsyncSnapshot<T> {
+  AsyncValue<T> get toAsyncValue {
+    if (hasError) {
+      return AsyncError<T>(error!, stackTrace);
+    }
 
-//     if (hasData) {
-//       return AsyncData(data!);
-//     }
+    // TODO: This isn't tested and possibly has subtle bugs
+    if (connectionState == ConnectionState.waiting ||
+        (connectionState == ConnectionState.none && !hasData)) {
+      return const AsyncLoading();
+    }
 
-//     throw StateError('I did not account properly for all the posssible states');
-//   }
-// }
+    if (hasData) {
+      return AsyncData(data!);
+    }
+
+    throw StateError('I did not account properly for all the posssible states');
+  }
+}
